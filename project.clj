f7e59807712c897d3bf9f5fd6a3ec45d47505922
Matthew Huebert <no-email@gmail.com>
2017-09-22 17:22:59 +0200

    lein-release plugin: bumped version from 0.1.1 to 0.1.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cells "0.1.1"
+(defproject cells "0.1.2-SNAPSHOT"
   :description "Interactive async in ClojureScript."
   :url "https://www.github.com/braintripping/cells"
   :license {:name "Mozilla Public License 2.0"
