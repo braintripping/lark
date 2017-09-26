411b2dc8a0a6711e220a73e19dda0b939bdf515e
Matthew Huebert <no-email@gmail.com>
2017-09-27 00:42:15 +0200

    Add project.clj descriptions

--- commands/project.clj
+++ commands/project.clj
@@ -2,6 +2,8 @@
 
   :url "https://www.github.com/braintripping/lark/tree/master/commands"
 
+  :description "Library for mapping keybindings to context-sensitive commands."
+
   :license {:name "Mozilla Public License 2.0"
             :url  "https://www.mozilla.org/en-US/MPL/2.0/"}
 
