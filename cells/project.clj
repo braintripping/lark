5a87c677bbbff194c44d5b45c773e897c487d3f7
Matthew Huebert <no-email@gmail.com>
2017-10-01 23:00:20 +0200

    lein-release plugin: bumped version from 0.1.3 to 0.1.4-SNAPSHOT for next development cycle

--- cells/project.clj
+++ cells/project.clj
@@ -1,4 +1,4 @@
-(defproject lark/cells "0.1.3"
+(defproject lark/cells "0.1.4-SNAPSHOT"
   :description "Interactive async in ClojureScript."
   :url "https://www.github.com/braintripping/lark/tree/master/cells"
   :license {:name "Mozilla Public License 2.0"
