8a7b4429913714c7b571e29611d0ac3fb4a7edd2
Matthew Huebert <no-email@gmail.com>
2017-10-18 20:21:14 +0200

    lein-release plugin: preparing 0.1.15 release

--- tools/project.clj
+++ tools/project.clj
@@ -1,4 +1,4 @@
-(defproject lark/tools "0.1.15-SNAPSHOT"
+(defproject lark/tools "0.1.15"
 
   :description "Open-source components of the Lark editor"
 
--- tools/src/deps.cljs
+++ tools/src/deps.cljs
@@ -1,3 +1,3 @@
-{:npm-deps {"keypress.js" "braintripping/Keypress#1442b0a"
+{:npm-deps {"@braintripping/keypress.js" "braintripping/Keypress#4477249"
             "codemirror"  "^5.30.0"}
  :externs  ["externs/lark.commands.ext.js"]}
