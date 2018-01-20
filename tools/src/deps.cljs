a47f8a5a9a83ca29af90f9323fbc5bb2a453a17a
Matthew Huebert <mhuebert@gmail.com>
2018-01-20 18:18:33 +0100

    deps(keypress)

--- tools/src/deps.cljs
+++ tools/src/deps.cljs
@@ -1,3 +1,3 @@
-{:npm-deps {"@braintripping/keypress.js" "github:braintripping/Keypress#6b3cc5b1"
+{:npm-deps {"@braintripping/keypress.js" "github:braintripping/Keypress#bbcfa371"
             "codemirror"  "^5.30.0"}
  :externs  ["externs/lark.commands.ext.js"]}
